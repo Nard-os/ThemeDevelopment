7dcd83001eb7bae55cb154349562898635c8e510
Nard-os <39374399+Nard-os@users.noreply.github.com>
2020-01-25 06:30:55 -0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,4 +1,67 @@
-<?php
+<?php 
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wedding
+ */
 
 ?>
-<?php wp_footer() ?>
+
+</div><!-- #content -->
+</div>
+	<footer id="colophon" class="site-footer">
+
+	<div id="footer-sidebar" class="row secondary">
+        <div class="col-md-3" id="footer-sidebar1">
+        <?php
+        if(is_active_sidebar('footer-sidebar-1')){
+        dynamic_sidebar('footer-sidebar-1');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar2">
+        <?php
+        if(is_active_sidebar('footer-sidebar-2')){
+        dynamic_sidebar('footer-sidebar-2');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar3">
+        <?php
+        if(is_active_sidebar('footer-sidebar-3')){
+        dynamic_sidebar('footer-sidebar-3');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar4">
+        <?php
+        if(is_active_sidebar('footer-sidebar-4')){
+        dynamic_sidebar('footer-sidebar-4');
+        }
+        ?>
+        </div>
+      </div>
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wedding' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'wedding' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wedding' ), 'wedding', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
